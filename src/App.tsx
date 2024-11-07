8c8c72e18fddb99da7deb7e2c54fa01cdbadb39d
서재원 <cinos81@gmail.com>
2024-11-07 17:02:51 +0900

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import ReactQuill from "react-quill";
+import { useSearchParams } from "react-router-dom";
+import 'react-quill/dist/quill.snow.css';
+
+function App() {
+  const [params, setParams] = useSearchParams()
+  const value = params.get('value') || ''
+  const onChange = (value: string) => {
+    setParams({ value })
+  }
+
+  const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ 'color': [] }, { 'background': [] }],
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'align': [] }],
+      ['link', 'image', 'video'],
+      ['clean']
+    ]
+  };
+
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike',
+    'color', 'background',
+    'list', 'bullet',
+    'align',
+    'link', 'image', 'video'
+  ];
+
+  return (
+    <ReactQuill 
+      theme="snow" 
+      value={value} 
+      onChange={onChange}
+      modules={modules}
+      formats={formats}
+      placeholder="내용을 입력하세요..."
+      preserveWhitespace={true}
+      style={{ height: '400px' }}
+    />
+  );
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<App />} />));
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
